70477f11aa39028952616dade81fb4ef534138c7
王雅靓 <wangyaliang@wangyalangdeMBP.etolli.com>
2018-10-27 22:57:09 -0700

    userInfo revised

--- userInfo.js
+++ userInfo.js
@@ -5,6 +5,7 @@ var express=require('express');
 var mysql=require('mysql');
 var app=express();
 
+
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -15,34 +16,52 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
+
+var openid='';
+var table_name='';
+var exactDate='';
+var table_name_sentence='';
+var table_name_keyWord='';
+
 app.post('/start',function(req,res){
 
-	var chunk = "";
-	var openid='';
-	var table_name='';
-	var exactDate='';
-
     req.on('data',function(data){
+    	var chunk = "";
     	chunk += data;
     	var dataJson=JSON.parse(chunk);
     	console.log('openid of the user：'+ dataJson.openid); 
     	openid=dataJson.openid;
+    	var firstKeyWord=dataJson.keyWord1;
     	var nowDate=new Date();
     	var exactDate=nowDate.toLocaleDateString()+" "+nowDate.toLocaleTimeString();
-    	table_name = exactDate+' '+ openid;
+    	table_name_sentence = 'sentence_'+exactDate+' '+ openid;
+    	table_name_keyWord = 'keyWord_'+exactDate+' '+ openid;
     	
-    	console.log('Name of the table:'+ table_name);
-    	
-    	var createTable="create table ?? (firstSentence varchar(255), secondSentence varchar(255), thirdSentence varchar(255))";
-
-    	connection.query(createTable, [table_name], function(err,result){
+    	//create a table for the sentences
+    	var createTable="create table ?? (id int primary key auto_increment, sentence varchar(255))";
+    	connection.query(createTable, [table_name_sentence], function(err,result){
            if (err) throw err;
-    		console.log('table created successfully!');
+    		console.log('table for sentences created successfully!');
          })
 
+    	//create a table for the key words
+    	var createTable="create table ?? (id int primary key auto_increment, keyWord varchar(255))";
+    	connection.query(createTable, [table_name_keyWord], function(err,result){
+           if (err) throw err;
+    		console.log('table for key words created successfully!');
+         })
+
+
+    	//record this event
     	connection.query('insert into event (openid, time) values (?,?)', [openid,exactDate], function(err,result){
     		 if (err) throw err;
     		console.log('event recorded!');
+    	})
+
+    	//insert first key word;
+    	connection.query('insert into ?? (keyWord) values (?)', [table_name_keyWord, firstKeyWord], function(err,result){
+    		 if (err) throw err;
+    		console.log('First key word recorded!');
     	})
 
     });
@@ -51,6 +70,48 @@ app.post('/start',function(req,res){
     res.end();
 
 })
+
+app.post('/insert',function(req,res){
+    var chunk = "";
+   
+    req.on('data',function(data){
+    chunk += data;
+    var dataJson=JSON.parse(chunk);
+
+    var newSentence=dataJson.sentence1;
+    var nextKeyWord=dataJson.nextKeyWord;
+
+    connection.query('insert into ?? (sentence) values (?)', [table_name_sentence, newSentence], function(err,result){
+    	if (err) throw err;
+    		console.log('New sentence inserted successfully!');
+    })
+
+    connection.query('insert into ?? (keyWord) values (?)', [table_name_keyWord, nextKeyWord], function(err,result){
+    	if (err) throw err;
+    		console.log('Next key word inserted successfully!');
+    })
+
+    console.log(dataJson.sentence1); 
+    });
+
+    res.writeHead(200,{"ContentType":"application/json;charset=utf-8"});
+    res.end();
+})
+
+
+var arrSentences=[];
+app.get('/show',function(req,res){
+	connection.query('select keyWord from ??', [table_name_keyWord], function(err,results){
+		if (err) throw err;
+		for (var i=0;i<results.length;i++){
+  		arrSentences[i]=results[i];
+  		console.log('key word selected:'+ arrSentences);
+    	res.send(arrSentences);	
+  }
+	})
+})
+
+
 
 console.log('Server running on 8084...');
 
